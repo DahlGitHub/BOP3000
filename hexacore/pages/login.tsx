41995e5182f4600857c7cf8866138116d8e853d5
Wohine <61017077+Wohine@users.noreply.github.com>
2023-01-30 15:56:54 +0100

    Login function with firebase auth

---
+++ hexacore/pages/login.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+import Layout from '../components/Layout'
+import SignIn from '../components/SignIn'
+
+const Login = () => (
+  <Layout title="About | Next.js + TypeScript Example">
+    <h1>Login</h1>
+    <SignIn/>
+  </Layout>
+)
+
+export default Login
---
+++ hexacore/pages/register.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+import Layout from '../components/Layout'
+import SignUp from '../components/SignUp'
+
+const Register = () => (
+  <Layout title="About | Next.js + TypeScript Example">
+    <h1>Register new account</h1>
+    <SignUp/>
+  </Layout>
+)
+
+export default Register
