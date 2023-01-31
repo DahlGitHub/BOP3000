ac7f46d0f45025f9dda4373f17e0fad49fa42e14
DahlGitHub <78036458+DahlGitHub@users.noreply.github.com>
2023-01-31 01:59:50 +0100

    Frontpage testing

--- hexacore/components/FrontPage.tsx
+++ hexacore/components/FrontPage.tsx
@@ -1,10 +1,41 @@
-import Link from 'next/link'
+import {Container, Grid, Card, Text } from "@nextui-org/react";
+import {useMediaQuery} from './useMediaQuery.js'
 
-const FrontPage = () => (
-  <>
-    <h1>Static frontpage</h1>
-    <p>This is the beginning of the end</p>
-  </>
-)
+const FrontPage = () => {
+  const isMd = useMediaQuery(960);
+
+  const MockItem = ({ text }) => {
+    return (
+      <Card css={{ h: "$20", $$cardColor: '$colors$primary' }}>
+        <Card.Body>
+          <Text h6 size={15} color="white" css={{ m: 0 }}>
+            {text}
+          </Text>
+        </Card.Body>
+      </Card>
+    );
+  };
+  return (
+    <Container lg>
+    <Grid.Container gap={2} justify="center">
+      <Grid xs={12} md={6}>
+        <MockItem text={isMd ? "1 of 1" : "1 of 2"} />
+      </Grid>
+      <Grid xs={6} md={6}>
+        <MockItem text={isMd ? "1 of 2" : "2 of 2"} />
+      </Grid>
+      <Grid xs={6} md={3}>
+        <MockItem text={isMd ? "2 of 2" : "1 of 3"} />
+      </Grid>
+      <Grid xs={6} md={3}>
+        <MockItem text={isMd ? "1 of 2" : "2 of 3"} />
+      </Grid>
+      <Grid xs={6} md={3}>
+        <MockItem text={isMd ? "2 of 2" : "3 of 3"} />
+      </Grid>
+    </Grid.Container>
+    </Container>
+  );
+}
 
 export default FrontPage
--- hexacore/components/Layout.tsx
+++ hexacore/components/Layout.tsx
@@ -1,6 +1,7 @@
 import React, { ReactNode } from 'react'
 import Link from 'next/link'
 import Head from 'next/head'
+import NavBar from './NavBar'
 
 type Props = {
   children?: ReactNode
@@ -15,12 +16,7 @@ const Layout = ({ children, title = 'This is the default title' }: Props) => (
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
     <header>
-      <nav>
-        <Link href="/">Home</Link> | <Link href="/about">About</Link> |{' '}
-        <Link href="/users">Users List</Link> |{' '}
-        <Link href="/login">Login</Link> |{' '}
-        <a href="/api/users">Users API</a>
-      </nav>
+      <NavBar/>
     </header>
     {children}
     <footer>
