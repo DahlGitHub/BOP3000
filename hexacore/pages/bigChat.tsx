c2635d32d473b584c05cfad5490393c02a9498d3
Linrik <henrik-lindam@hotmail.com>
2023-02-24 20:28:50 +0100

    authstate og chat
    lagt inn at meldinger blir opptdatert og lagt inn en hook for å sjekke state om bruker er logget inn eller ikke.
    fjernet også noen unødvendige filer

--- hexacore/pages/bigChat.tsx
+++ hexacore/pages/bigChat.tsx
@@ -1,15 +1,21 @@
-import { Input, Container  } from '@nextui-org/react';
+import { Container  } from '@nextui-org/react';
 import Layout from '../app/Layout';
 import Message from '../components/MessageInput'
 import Messages from '../components/MessageList'
-import { useState } from 'react';
+import { useRouter } from 'next/router';
+import { auth } from '../firebase';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { redirect } from 'next/navigation';
    
-/*
-const [messages, setMessages] = useState([])
-  for(var i = 0; i < 10; i++){
-    setMessages(messages => [...messages, 'hei'+i])
-  }*/
+
 export default function BigChat() {
+  const router = useRouter();
+  const [user, loading] = useAuthState(auth);
+
+  if(user){
+    console.log(user)
+    //redirect('/')
+  }
   
   return <Layout>
     <Container justify='center' className='flex flex-grow'>
