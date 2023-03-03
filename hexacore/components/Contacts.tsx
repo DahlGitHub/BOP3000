d75f1f1ff927171455daf67e9908df5b5ceef27b
Wohine <61017077+Wohine@users.noreply.github.com>
2023-03-03 14:13:35 +0100

    User search engine

--- hexacore/components/Contacts.tsx
+++ hexacore/components/Contacts.tsx
@@ -1,135 +1,138 @@
-import Link from 'next/link'
+import React from 'react';
 import Layout from '../app/Layout'
 import algoliasearch from "algoliasearch/lite";
-import { InstantSearch, SearchBox, Hits } from "react-instantsearch-dom";
-import ContactRow from './ContactRow';
-import { connectHits } from 'react-instantsearch-dom';
+import { InstantSearch, SearchBox, connectHits } from "react-instantsearch-dom";
 
 
-const searchClient = algoliasearch(
+const searchClient = algoliasearch (
   "FH6G88713S",
   "6821b8b5ec64e3c780fe083eb55e5a7d"
 );
 
 const Contacts = () => {
+  const [name, setName] = React.useState(null);
+  const [email, setEmail] = React.useState(null);
+  const [picture, setPicture] = React.useState(null);
 
-  const Hits = ({ hits }) => (
-  <>
-    {hits.map(hit => (
-    <tr>
-      
-        <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-            <div className="flex items-center">
-                <div className="flex-shrink-0 w-10 h-10">
-                <img className="w-full h-full rounded-full" src={hit.picture}alt="" />
+  const Hits = ({ hits }) => {
+
+    return (
+      <>
+        {hits.map(hit => (
+          
+          <tr>
+            
+            <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                <div className="flex items-center">
+                    <div className="flex-shrink-0 w-10 h-10">
+                    <img className="w-full h-full rounded-full" src={hit.picture}alt="" />
+                    </div>
+                    <div className="ml-3">
+                        <p className="text-gray-900 whitespace-no-wrap">
+                          {hit.name}
+                        </p>
+                    </div>
                 </div>
-                <div className="ml-3">
-                    <p className="text-gray-900 whitespace-no-wrap">
-                        {hit.name}
-                    </p>
-                </div>
-            </div>
-        </td>
-        <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-        <p className="text-gray-900 whitespace-no-wrap">Kroa i Bø</p>
-        </td>
-        <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-        <p className="text-gray-900 whitespace-no-wrap">
-            {hit.email}
-        </p>
-        </td>
-        <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-        <p className="text-gray-900 whitespace-no-wrap">
-            2
-        </p>
-        </td>
-        <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-            <span className="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
-                <span aria-hidden className="absolute inset-0 bg-green-200 opacity-50 rounded-full"></span>
-            <span className="relative">Active</span>
-        </span>
-        </td>
-      </tr>
-      ))}
-    </>
-  )
+            </td>
+            <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+              <p className="text-gray-900 whitespace-no-wrap">Kroa i Bø</p>
+            </td>
+            <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+              <p className="text-gray-900 whitespace-no-wrap">
+                {hit.email}
+              </p>
+            </td>
+            <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+              <p className="text-gray-900 whitespace-no-wrap">
+                2
+              </p>
+            </td>
+            <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+              <button className="bg-blue-600 font-semibold text-white p-2 w-32 rounded-full hover:bg-blue-700 focus:outline-none focus:ring shadow-lg hover:shadow-none transition-all duration-300 m-2">
+                Add contact
+              </button>
+            </td>
+          </tr>
+        ))}
+      </>
+    )  
+  }
 
   const CustomHits = connectHits(Hits);
 
   return (
     <Layout title="Hexacore">
       <InstantSearch searchClient={searchClient} indexName="users">
-      <div className="bg-white p-8 rounded-md w-full">
-        <div className=" flex items-center justify-between pb-6">
-          <div>
-            <h2 className="text-gray-600 font-semibold">Contacts</h2>
-            <span className="text-xs">Add more contacts?</span>
-          </div>
-          <div className="flex items-center justify-between">
-            <div className="flex bg-gray-50 items-center p-2 rounded-md">
-              
-              <SearchBox translations={{placeholder: 'Search for users'}}/>
+        <div className="bg-white p-8 rounded-md w-full">
+          <div className=" flex items-center justify-between pb-6">
+            <div>
+              <h2 className="text-gray-600 font-semibold">Contacts</h2>
+              <span className="text-xs">Add more contacts?</span>
+            </div>
+            <div className="flex items-center justify-between">
+              <div className="flex bg-gray-50 items-center p-2 rounded-md">
+                <SearchBox translations={{placeholder: 'Search for users'}}/>
+              </div>
+                <div className="lg:ml-40 ml-10 space-x-8">
+                  <button className="bg-indigo-600 px-4 py-2 rounded-md text-white font-semibold tracking-wide cursor-pointer">Add contacts</button>
+                  <button className="bg-indigo-600 px-4 py-2 rounded-md text-white font-semibold tracking-wide cursor-pointer">Manage contacts</button>
                 </div>
-              <div className="lg:ml-40 ml-10 space-x-8">
-                <button className="bg-indigo-600 px-4 py-2 rounded-md text-white font-semibold tracking-wide cursor-pointer">Add contacts</button>
-                <button className="bg-indigo-600 px-4 py-2 rounded-md text-white font-semibold tracking-wide cursor-pointer">Manage contacts</button>
               </div>
             </div>
-          </div>
-          <div>
-            <div className="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
-              <div className="inline-block min-w-full shadow rounded-lg overflow-hidden">
-                <table className="min-w-full leading-normal">
-                  <thead>
-                    <tr>
-                      <th
-                        className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
-                        Name
-                      </th>
-                      <th
-                        className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
-                        Workplace
-                      </th>
-                      <th
-                        className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
-                        Email
-                      </th>
-                      <th
-                        className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
-                        Mutual groups
-                      </th>
-                      <th
-                        className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
-                        Status
-                      </th>
-                    </tr>
-                  </thead>
-                  
-                  <tbody>
-                    <CustomHits/>
-                  </tbody>  
-                </table>
-                <div
-                  className="px-5 py-5 bg-white border-t flex flex-col xs:flex-row items-center xs:justify-between          ">
-                  <span className="text-xs xs:text-sm text-gray-900">
-                                  Showing 1 to 4 of 50 Entries
-                              </span>
-                  <div className="inline-flex mt-2 xs:mt-0">
-                    <button
-                                      className="text-sm text-indigo-50 transition duration-150 hover:bg-indigo-500 bg-indigo-600 font-semibold py-2 px-4 rounded-l">
-                                      Prev
-                                  </button>
-                    &nbsp; &nbsp;
-                    <button
-                                      className="text-sm text-indigo-50 transition duration-150 hover:bg-indigo-500 bg-indigo-600 font-semibold py-2 px-4 rounded-r">
-                                      Next
-                                  </button>
+            <div>
+              <div className="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
+                <div className="inline-block min-w-full shadow rounded-lg overflow-hidden">
+                  <table className="min-w-full leading-normal">
+                    <thead>
+                      <tr>
+                        <th
+                          className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                          Name
+                        </th>
+                        <th
+                          className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                          Workplace
+                        </th>
+                        <th
+                          className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                          Email
+                        </th>
+                        <th
+                          className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                          Mutual groups
+                        </th>
+                        <th
+                          className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                          Add contact
+                        </th>
+                      </tr>
+                    </thead>
+                    
+                    <tbody>
+                      <CustomHits/>
+                    </tbody>  
+                  </table>
+                  <div
+                    className="px-5 py-5 bg-white border-t flex flex-col xs:flex-row items-center xs:justify-between          ">
+                    <span className="text-xs xs:text-sm text-gray-900">
+                                    Showing 1 to 4 of 50 Entries
+                                </span>
+                    <div className="inline-flex mt-2 xs:mt-0">
+                      <button
+                                        className="text-sm text-indigo-50 transition duration-150 hover:bg-indigo-500 bg-indigo-600 font-semibold py-2 px-4 rounded-l">
+                                        Prev
+                                    </button>
+                      &nbsp; &nbsp;
+                      <button
+                                        className="text-sm text-indigo-50 transition duration-150 hover:bg-indigo-500 bg-indigo-600 font-semibold py-2 px-4 rounded-r">
+                                        Next
+                                    </button>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
         </InstantSearch>
     </Layout>
   )
--- hexacore/components/DetailsForm.tsx
+++ hexacore/components/DetailsForm.tsx
@@ -1,18 +1,14 @@
 import React, { Fragment } from "react";
-import {Container, Image, Row, Col, Grid, Card, Text } from "@nextui-org/react";
-import {useMediaQuery} from './useMediaQuery.js'
+import {Container} from "@nextui-org/react";
 import {auth, db, storage} from "../firebase"
 import { doc, setDoc, addDoc, getDoc } from "firebase/firestore";
 import { useRouter } from 'next/navigation';
 import { ref, getDownloadURL, uploadBytesResumable } from "firebase/storage";
 
 const DetailsForm = () => {
-    const isMd = useMediaQuery(960);
     const [email, setEmail] = React.useState(null);
     const [country, setCountry] = React.useState(null);
-    const [language, setLanguage] = React.useState(null);
     const [firstName, setFirstName] = React.useState(null);
-    const [lastName, setLastName] = React.useState(null);
     const [phone, setPhone] = React.useState(null);
     const router = useRouter();
     const [fileUrl, setFileUrl] = React.useState(null);
@@ -27,7 +23,6 @@ const DetailsForm = () => {
             const docData = {
                 email: email,
                 country: country,
-                language: language,
                 name: firstName,
                 phone: phone,
                 picture: fileUrl
@@ -122,33 +117,8 @@ const DetailsForm = () => {
                                         </div>
                                     </div>
                                 </div>
+                                
                                 <div className='w-full md:w-full px-3 mb-6'>
-                                    <label className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'>Preferred language</label>
-                                    <div className="flex-shrink w-full inline-block relative">
-                                        <select onChange={e => { setLanguage(e.currentTarget.value); }} className="block appearance-none text-gray-600 w-full bg-white border border-gray-400 shadow-inner px-4 py-2 pr-8 rounded">
-                                            <option>choose ...</option>
-                                            <option>English</option>
-                                            <option>France</option>
-                                            <option>Spanish</option>
-                                        </select>
-                                        <div className="pointer-events-none absolute top-0 mt-3  right-0 flex items-center px-2 text-gray-600">
-                                            <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="personal w-full border-t border-gray-400 pt-4">
-                                    <h2 className="text-2xl text-gray-900">Personal info:</h2>
-                                    <div className="flex items-center justify-between mt-4">
-                                        <div className='w-full md:w-1/2 px-3 mb-6'>
-                                            <label className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2' >first name</label>
-                                            <input onChange={e => { setFirstName(e.currentTarget.value); }} className='appearance-none block w-full bg-white text-gray-700 border border-gray-400 shadow-inner rounded-md py-3 px-4 leading-tight focus:outline-none  focus:border-gray-500' type='text'  required/>
-                                        </div>
-                                        <div className='w-full md:w-1/2 px-3 mb-6'>
-                                            <label className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2' >last name</label>
-                                            <input onChange={e => { setLastName(e.currentTarget.value); }} className='appearance-none block w-full bg-white text-gray-700 border border-gray-400 shadow-inner rounded-md py-3 px-4 leading-tight focus:outline-none  focus:border-gray-500' type='text'  required/>
-                                        </div>
-                                    </div>
-                                    <div className='w-full md:w-full px-3 mb-6'>
                                         <label className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'>Phone number</label>
                                         <input type="number" onChange={e => { setPhone(e.currentTarget.value); }} className='appearance-none block w-full bg-white text-gray-700 border border-gray-400 shadow-inner rounded-md py-3 px-4 leading-tight focus:outline-none  focus:border-gray-500'  required/>
                                     </div>
@@ -159,7 +129,7 @@ const DetailsForm = () => {
                                     <div className="flex justify-end">
                                         <button onClick={submit} className="appearance-none bg-gray-200 text-gray-900 px-2 py-1 shadow-sm border border-gray-400 rounded-md mr-3">save changes</button>
                                     </div>
-                                </div>
+                                
                             </div>
                         </form>
                     </div>
