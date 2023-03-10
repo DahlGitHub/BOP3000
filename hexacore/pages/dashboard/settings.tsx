dea38443a7a43f04d5672ef1df66bb4e8ca487c5
DahlGitHub <78036458+DahlGitHub@users.noreply.github.com>
2023-03-10 13:55:21 +0100

    added settings to sidebav

---
+++ hexacore/pages/dashboard/settings.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import DashboardLayout from '../../components/DashboardLayout';
+import UserSettings from '../../components/userSettings';
+
+const settings = () => {
+  return (
+    <DashboardLayout>
+        <UserSettings/>
+    </DashboardLayout>
+  )
+}
+
+export default settings
