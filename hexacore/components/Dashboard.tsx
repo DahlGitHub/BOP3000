4ac97d62fc2458f680d3c99c5b1b6dac198e3f69
DahlGitHub <78036458+DahlGitHub@users.noreply.github.com>
2023-03-10 13:58:24 +0100

    added and moved usersettings

--- hexacore/components/Dashboard.tsx
+++ hexacore/components/Dashboard.tsx
@@ -6,7 +6,7 @@ import Link from 'next/link';
 import Logo from "/public/images/hexacore.png";
 import { useRouter } from "next/router";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUser, faUserFriends } from "@fortawesome/free-solid-svg-icons";
+import { faGears, faUser, faUserFriends } from "@fortawesome/free-solid-svg-icons";
 import ProfileDropdown from "./Layout/NavComponents/ProfileDropdown";
 
 const SidebarItems = [
@@ -68,12 +68,12 @@ const Dashboard = () => {
           )})}
           </ul>
          <hr className="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700"/>
-         <ul className='bottom-0 absolute w-full pr-6'>
+         <ul className='w-full'>
          <li>
-            <a href="#" className=" flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
-               <svg aria-hidden="true" className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 3a1 1 0 00-1 1v12a1 1 0 102 0V4a1 1 0 00-1-1zm10.293 9.293a1 1 0 001.414 1.414l3-3a1 1 0 000-1.414l-3-3a1 1 0 10-1.414 1.414L14.586 9H7a1 1 0 100 2h7.586l-1.293 1.293z" clip-rule="evenodd"></path></svg>
+            <Link href="/dashboard/settings" className=" flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+              <FontAwesomeIcon icon={faGears} />
                <div className="flex-1 ml-3">Settings</div>
-            </a>
+            </Link>
          </li>
          
       </ul>
