46d1eef8e8253358c750a85b8051a35084dbf889
DahlGitHub <78036458+DahlGitHub@users.noreply.github.com>
2023-03-14 19:23:43 +0100

    team create testing

--- hexacore/components/Team/CreateTeam.tsx
+++ hexacore/components/Team/CreateTeam.tsx
@@ -2,23 +2,28 @@ import {v4 as uuid} from 'uuid';
 import React, { useState, useEffect} from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFolderPlus } from '@fortawesome/free-solid-svg-icons';
+import {Text } from "@nextui-org/react";
+import { TestTeam } from './TestTeam';
 
 const CreateTeam = () => {
 
   const id = uuid().replaceAll('-','');
   const [newId, setNewId] = useState('');
 
-  return (
-    <>
-    <div>
-    <button type="button" onClick={() => setNewId(id)} className="text-white p-2 bg-[#24292F] hover:bg-[#24292F]/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30 mr-2 mb-2">
-    <FontAwesomeIcon className='pr-2' icon={faFolderPlus}/>
-    Create a team
+  const [components, setTeam] = useState([]);
 
-    </button>      
-    <span>{newId}</span>  
+  function addTeam() {
+    setTeam([...components, () => setNewId(id)])
+  }
+
+  return (
+    <div>
+      <button type="button" onClick={addTeam} className="text-white p-2 bg-[#24292F] hover:bg-[#24292F]/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30 mr-2 mb-2">
+        <FontAwesomeIcon className='pr-2' icon={faFolderPlus}/>
+        Create a team
+      </button>      
+      {components.map((item, i) => ( <TestTeam text={item}/> ))}
     </div>
-    </>
   )
 }
 
---
+++ hexacore/components/Team/TestTeam.tsx
@@ -0,0 +1,13 @@
+import { Text } from '@nextui-org/react';
+import React from 'react';
+
+const TestTeam = (props) => {
+
+    return (
+        <div>
+            <Text>{props.text}</Text>
+        </div>
+    );
+
+};
+export {TestTeam}
