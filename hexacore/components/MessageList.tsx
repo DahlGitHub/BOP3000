2bf33b52686d47c332529ddf48e937c46f061423
Linrik <henrik-lindam@hotmail.com>
2023-03-14 16:20:49 +0100

    Chat
    chatten henter brukerdataen og viser det

--- hexacore/components/MessageList.tsx
+++ hexacore/components/MessageList.tsx
@@ -8,65 +8,50 @@ const qMessages = query(collection(db, '/groups/e5UQ87CZktE0ewgqvWpx/Channel/Hex
 const qChatters = query(collection(db, '/groups/e5UQ87CZktE0ewgqvWpx/Members/'))
 export default () =>{
     const [messages, setMessages] = useImmer([])
-    const chattersID = useRef(new Map())
-    const chatters = useRef(new Map())
-    //const [chatters, setChatters] = useImmer([])
+    let chatters = useRef(new Map())
+    //const [chatters, setChatters] = useImmer(new Map())
 
-
-    useEffect(()=>{
-        
-        const getID = async ()=>{
-        const querySnapshot = await getDocs(qChatters);
-        querySnapshot.forEach(async(id) => {
-            if(!chattersID.current.has(id.data())){
-               chattersID.current.set(id.data(), id.data())
-            }
-        });
-        getPerson()
-        }
-        getID()
-        const queryUser = query(collection(db, 'users'), where('uid', '==', chattersID.current.forEach))
-
-        const getPerson = () =>{  
-            chattersID.current.forEach(async(id)=>{
-            const person = await getDoc(doc(db, 'users', id.uid))
-            //
-            chatters.current.set(person.data().uid, person.data())
+    /*const getPeople =async () => {
+        const queryChatters = await getDocs(qChatters);
+        queryChatters.forEach(async (id)=>{
+            const data = (await getDoc(doc(db, 'users', id.data().uid))).data()
+            chatters.current.set(data.uid, data)
         })
-        } 
+    }*/
+    const getPeople = async () => {
+        const queryChatters = await getDocs(qChatters);
+        const chattersMap = new Map();
         
-
-        
-        
-        
-
-        onSnapshot(qMessages, (querySnapshot) =>{
-            querySnapshot.docChanges().forEach(async (change)=>{
-                //løsning 1
-                //hente alle nrukere i chatten og plassere de på sin melding
-                //løsning 2
-                //hent bruker data fra bruker id
-                //har brukeren blitt hentet aleredet bruk den dataen
-                //if(!chatters.includes(change.doc.data())){
-                    
-                    //trenger å catche om brukeren ikke eksisterer lengre
-                //}
-                console.log(chatters.current.get(change.doc.data().uid))
-                const connect = {
-                    user: chatters.current.get(change.doc.data().uid),
-                    message: change.doc.data()
-                }
-
-                if(change.type === 'added'){
-                    setMessages(messages => [...messages, connect])
-                }
-                if(change.type === 'modified'){
-                    // logikk for å endre meldingen som har blitt endret
-                    setMessages(messages)
-                }
-                if(change.type === 'removed'){
-                    //logikk som fjerner meldingen
-                }
+        for (const docSnap of queryChatters.docs) {
+          const id = docSnap.data().uid;
+          const docRef = doc(db, 'users', id);
+          const snapshot = await getDoc(docRef);
+          const data = snapshot.data();
+          chattersMap.set(id, data);
+        }
+      
+        return chattersMap;
+      }
+    useEffect(()=>{
+        //chatters.current = getPeople()
+        const chattersmap = getPeople().then((data)=>{
+            onSnapshot(qMessages, async (querySnapshot) =>{
+                await querySnapshot.docChanges().forEach(async (message)=>{  
+                    const connect = {
+                        user: data.get(message.doc.data().uid),
+                        message: message.doc.data()
+                    }
+                    if(message.type === 'added'){
+                        setMessages(messages => [...messages, connect])
+                    }
+                    if(message.type === 'modified'){
+                        // logikk for å endre meldingen som har blitt endret
+                        setMessages(messages)
+                    }
+                    if(message.type === 'removed'){
+                        //logikk som fjerner meldingen
+                    }
+                })
             })
         })
     }, [])
