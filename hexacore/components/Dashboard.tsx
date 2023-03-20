bc9881228f0608bc52a9ac4fd3efaebc28fcf766
Wohine <61017077+Wohine@users.noreply.github.com>
2023-03-20 16:01:59 +0100

    Removed videochat from nav

--- hexacore/components/Dashboard.tsx
+++ hexacore/components/Dashboard.tsx
@@ -28,7 +28,6 @@ const Dashboard = () => {
 
   return ( 
   <>
-  
 <nav className="absolute top-0 z-50 w-full bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
   <div className="px-3 py-3 lg:px-5 lg:pl-3">
     <div className="flex items-center justify-between">
--- hexacore/components/VideoChat/VideoChat.tsx
+++ hexacore/components/VideoChat/VideoChat.tsx
@@ -3,8 +3,7 @@
 const VideoChat = () => {
     return(
        <> 
-        <div id="otEmbedContainer" className="w-fit h-fit"></div>
-        <script src="https://tokbox.com/embed/embed/ot-embed.js?embedId=fea87c55-a335-4e63-9d25-c8a3240dd856&room=DEFAULT_ROOM"></script>
+        
         </>
     )
 }
