3a6420b2eed3aeb85844116945802f7eb32e4eb6
Linrik <henrik-lindam@hotmail.com>
2023-03-21 13:11:32 +0100

    lagt inn usercontext for dashboard

--- hexacore/context/UserContext.tsx
+++ hexacore/context/UserContext.tsx
@@ -2,9 +2,4 @@ import { useContext, createContext } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../firebase";
 
-
-
-export const UserContext = () => {
-    const [user, loading] = useAuthState(auth);
-    const userContext = createContext(user);
-}
+export const UserContext = createContext(null)
