e9e6551fee4219425a2b8f2dda104a32adafea24
DahlGitHub <78036458+DahlGitHub@users.noreply.github.com>
2023-03-23 12:44:32 +0100

    testing chat styling

--- hexacore/components/Contact/ContactList.tsx
+++ hexacore/components/Contact/ContactList.tsx
@@ -103,7 +103,7 @@ const ContactList = ({ isOpen, onClose, onOpen }) => {
             </div>
         </div>
         {}
-        <div className="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
+        <div className="">
             {chat.length > 0
             ? <Chat chatID={chat}/>
             : <p>hei</p>
