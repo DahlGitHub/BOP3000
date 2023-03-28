e2e25c88da43bdf0c7728edd205b722acd01fced
DahlGitHub <78036458+DahlGitHub@users.noreply.github.com>
2023-03-28 15:55:14 +0200

    new layout structure

---
+++ hexacore/components/Layout/Dashboard/DashboardLayout.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import Dashboard from "./Dashboard";
+import { UserContext } from "../../../context/UserContext";
+import { auth } from "../../../firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { Progress } from "@nextui-org/react";
+import DashboardHead from "./DashboardHead";
+import DashboardSidebar from "./DashboardSidebar";
+
+
+const DashboardLayout = ({ children }) => {
+  const [user, loading, error] = useAuthState(auth)
+  return (
+    <UserContext.Provider value={{user, loading, error}}>
+      <div className="min-h-screen w-full">
+      <DashboardHead />
+      <div className="flex">
+        <DashboardSidebar />
+      {loading
+      ? <Progress
+      indeterminated
+      value={50}
+      color="secondary"
+      status="secondary"/>
+
+      :
+      
+      <div className="w-full p-4">
+        { children }
+      </div>
+      
+      }
+      </div>
+    </div>
+    </UserContext.Provider>
+    
+  );
+};
+
+export default DashboardLayout;
