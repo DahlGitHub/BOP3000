4c697b07515cc156f02a7552e4b9585d49c33d7e
DahlGitHub <78036458+DahlGitHub@users.noreply.github.com>
2023-03-30 14:01:57 +0200

    drawer button show / close

--- hexacore/components/Drawer.tsx
+++ hexacore/components/Drawer.tsx
@@ -1,3 +1,5 @@
+import { faArrowRight, faArrowRotateRight, faChevronLeft, faChevronRight } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 
 
@@ -12,18 +14,22 @@ const Drawer = ({mainContent, title, isOpen, open, close}) => {
                 isOpen ? 'block' : 'hidden'
             } `}
             >
-                <div className="h-[calc(100vh-70px)] w-auto py-8 overflow-y-auto bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700">
-                    
+                <div className="absolute mx-2 my-3 left-15 z-50">
+                        <button onClick={close} className="w-full items-center p-1 mx-1 text-center bg-gray-200 text-[14px] font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-600 dark:bg-gray-700">
+                        <FontAwesomeIcon icon={faChevronLeft}/>
+                        </button>
+                    </div>
+                <div className="h-[calc(100vh-70px)] w-auto py-4 bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700">
+                
                     <h1 className="text-center text-lg leading-6 font-medium text-gray-900 dark:text-white">{title}</h1>
                     
                     <div className="mt-8 space-y-4">
 
                         {mainContent}
-
-                        <button onClick={close} className="w-full items-center p-2 text-center font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
-                            Hide
-                        </button>
+                
+                        
                     </div>
+                    
                 </div>
             </div>
             <div
@@ -31,13 +37,11 @@ const Drawer = ({mainContent, title, isOpen, open, close}) => {
                 isOpen ? 'hidden' : 'block'
             } `}
             >
-                <div className="max-h-fit w-auto py-8 overflow-y-auto bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700">
-                    <div className="mt-8 p-3">
-                        <button onClick={open} className="w-full items-center p-2 text-center font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
-                            Show {title}
+                    <div className="absolute mx-2 my-3 left-15 z-50">
+                        <button onClick={open} className="w-full items-center p-1 mx-1 text-center bg-gray-200 text-[14px] font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-600 dark:bg-gray-700">
+                        <FontAwesomeIcon icon={faChevronRight}/>
                         </button>
                     </div>
-                </div>
             </div>
         </div>
     )
