f8e8b0c65eada4bc6019e609fd1d25c33d10f914
Linrik <henrik-lindam@hotmail.com>
2023-04-13 16:19:09 +0200

    Revert "Merge branch 'KanbanDynamisk' of https://github.com/DahlGitHub/hexacore"
    This reverts commit 4ed8817c903c4d86fd309eb0adc6f6abb9ae2bc2, reversing
    changes made to f8347ff89e4ce8381c252f122a433c113b2bab19.

--- hexacore/components/Contact/ContactList.tsx
+++ hexacore/components/Contact/ContactList.tsx
@@ -12,39 +12,6 @@ const ContactList = () => {
   const [contacts, setContacts] = React.useState([]);
   const docImport = doc;
 
-    const [chat, setChat] = useState('')
-
-    useEffect(() => {
-        async function fetchRequests() {
-            const querySnapshot = await getDocs(collection(db, "users", auth.currentUser?.uid, "contacts"));
-          
-            const promises = querySnapshot.docs.map(async (doc) => {
-              const userId = doc.data().uid;
-              const userDoc = await getDoc(docImport(db, "users", userId));
-              const userData = userDoc.data();
-              const chatID = [auth.currentUser.uid.toLowerCase(), userId.toLowerCase()].sort().join('')
-            
-              const element = (
-                <button key={doc.id} onClick={()=>setChat(chatID)} className="flex items-center w-full px-5 py-2 transition-colors duration-200 dark:hover:bg-gray-800 gap-x-2 hover:bg-gray-100 focus:outline-none">
-                  <img className="object-cover w-8 h-8 rounded-full" src={userData.picture} alt=""/>
-                  <div className="text-left rtl:text-right">
-                    <h1 className="text-sm font-medium text-gray-700 capitalize dark:text-white">{userData.name}</h1>
-                    <p className="text-xs text-gray-500 dark:text-gray-400">{userData.email}</p>
-                  </div>
-                </button>
-              );
-          
-              return element;
-            });
-          
-            const results = await Promise.all(promises);
-            setContacts(results);
-        }
-      
-        fetchRequests();
-      }, []); // Run this effect only once on component mount
-  
-
   const [chat, setChat] = useState('')
     onAuthStateChanged(auth, (user) => {
     if (user) {
--- hexacore/components/Kanban/Kanban.tsx
+++ hexacore/components/Kanban/Kanban.tsx
@@ -1,151 +1,208 @@
 import React from 'react'
+
+import Image from "next/dist/client/image";
+import {
+  ChevronDownIcon,
+  PlusIcon,
+  
+  PlusCircleIcon,
+} from '@heroicons/react/24/outline'
+import CardItem from "./CardItem";
+import BoardData from "./board-data.json";
 import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 import { useEffect, useState } from "react";
-import { Button, Dropdown, Input } from "@nextui-org/react";
-import { collection, doc, getDocs, onSnapshot, orderBy, query, setDoc, updateDoc } from 'firebase/firestore';
-import { db } from '../../firebase';
-import { useImmer } from 'use-immer';
-import Board from './Board';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser, faUserFriends } from "@fortawesome/free-solid-svg-icons";
 
-export function createGuidId() {
+function createGuidId() {
   const randomNumber = Math.floor(Math.random() * 1000000000000);
   return randomNumber;
 }
 
-// groups/a82bcf3fff364e71b2a8bb39903be3dd/kanbanid/dokumentid
 export default function Home() {
-  const [ready, setReady] = useState(false)
-  const [boardData, setBoardData] = useState ([])
-  const [newBoard, setNewBoard] = useState('')
-  const [members, setMembers] = useImmer([])
+  const [ready, setReady] = useState(false);
+  const [boardData, setBoardData] = useState(BoardData);
+  const [showForm, setShowForm] = useState(false);
+  const [selectedBoard, setSelectedBoard] = useState(0);
 
-  const qMembers = query(collection(db, '/groups/a82bcf3fff364e71b2a8bb39903be3dd/members'))
-
-  const getMembers = async () => {
-    const members = await getDocs(qMembers)
-    const membersData = members.docs.map((doc) => {
-      return doc.data()
-    })
-    setMembers(membersData)
-  }
   useEffect(() => {
-    getMembers()
-    const q = query(collection(db, 'groups/a82bcf3fff364e71b2a8bb39903be3dd/kanbanid'), orderBy('order', 'asc'))
-    onSnapshot(q, (snapshot) => {
-      snapshot.docChanges().forEach((change) => {
-        if (change.type === 'added' || change.type === 'modified') {
-          setBoardData((boardData) => {
-            const newData = change.doc.data();
-            const newBoardData = [...boardData];
-            newBoardData[change.newIndex] = newData;
-            return newBoardData;
-          });
-        }
-        if (change.type === 'removed') {
-          setBoardData((boardData) => {
-            const newBoardData = [...boardData];
-            newBoardData.splice(change.oldIndex, 1);
-            return newBoardData;
-          })
-          //må oppdatere order på alle boards
-        }
-      });
-    })
-    if (process) {
+    if (process.browser) {
       setReady(true);
     }
   }, []);
 
-  const onDragEnd = async (re) => {
+  const onDragEnd = (re) => {
     if (!re.destination) return;
-    if (re.type === 'BOARD') {
-      const dragItem = boardData[re.source.index];
-      const newBoardData = [...boardData];
-      newBoardData.splice(re.source.index, 1);
-      newBoardData.splice(re.destination.index, 0, dragItem);
-      setBoardData(newBoardData);
-      newBoardData.forEach(async (board, index) => {
-        await updateDoc(doc(db, 'groups/a82bcf3fff364e71b2a8bb39903be3dd/kanbanid', board.id), {
-          order: index
-        })
-      })
-    }else if(re.type === 'CARD'){
-      var dragItem = boardData[parseInt(re.source.droppableId)].items[re.source.index];
-      boardData[parseInt(re.source.droppableId)].items.splice(
-        re.source.index,
-        1
-      )
-      dragItem.boardId = boardData[parseInt(re.destination.droppableId)].id;
-      boardData[parseInt(re.destination.droppableId)].items.splice(
-        re.destination.index,
-        0,
-        dragItem
-      )
-      await updateDoc(doc(db, 'groups/a82bcf3fff364e71b2a8bb39903be3dd/kanbanid', boardData[parseInt(re.source.droppableId)].id), {
-        items: boardData[parseInt(re.source.droppableId)].items
-      })
-      await updateDoc(doc(db, 'groups/a82bcf3fff364e71b2a8bb39903be3dd/kanbanid', boardData[parseInt(re.destination.droppableId)].id), {
-        items: boardData[parseInt(re.destination.droppableId)].items
-      })
-    }
+    let newBoardData = boardData;
+    var dragItem =
+      newBoardData[parseInt(re.source.droppableId)].items[re.source.index];
+    newBoardData[parseInt(re.source.droppableId)].items.splice(
+      re.source.index,
+      1
+    );
+    newBoardData[parseInt(re.destination.droppableId)].items.splice(
+      re.destination.index,
+      0,
+      dragItem
+    );
+    setBoardData(newBoardData);
   };
 
-  const addBoard = async () => {
-    if(newBoard.length === 0) return;
-    const id = createGuidId().toString()
-    const setOrder = boardData.length === 0 ? 0 : boardData[boardData.length-1].order+1
-    await setDoc(doc(db, 'groups/a82bcf3fff364e71b2a8bb39903be3dd/kanbanid', id), {
-      id: id,
-      order: setOrder,
-      name: newBoard,
-      items: []
-    })
+  const onTextAreaKeyPress = (e) => {
+    if(e.keyCode === 13) //Enter
+    {
+      const val = e.target.value;
+      if(val.length === 0) {
+        setShowForm(false);
+      }
+      else {
+        const boardId = e.target.attributes['data-id'].value;
+        const item = {
+          id: createGuidId(),
+          title: val,
+          priority: 0,
+          chat:0,
+          attachment: 0,
+          assignees: []
+        }
+        let newBoardData = boardData;
+        newBoardData[boardId].items.push(item);
+        setBoardData(newBoardData);
+        setShowForm(false);
+        e.target.value = '';
+      }
+    }
   }
+
   return (
-      <div className="pt-20 pl-10 flex flex-col w-full h-screen">
+    
+      <div className="pt-20 pl-10 flex flex-col h-screen">
         {/* Board header */}
         <div className="flex flex-initial">
           <div className="flex items-center">
             <h4 className="text-4xl font-bold text-gray-600">Kanban board</h4>
+            <FontAwesomeIcon icon={faUser}
+              className="w-9 h-9 text-gray-500 rounded-full
+            p-1 bg-white ml-5 shadow-xl"
+            />
           </div>
-          <Input aria-label='addBoard' aria-hidden='false' value={newBoard} onChange={e => setNewBoard(e.target.value)} placeholder='Add list'></Input>
-          <Button className='text-black bg-indigo-800' onPress={addBoard}>Add</Button>
+
+          <ul className="flex space-x-3 ml-10">
+            <li>
+              <img
+                src="https://firebasestorage.googleapis.com/v0/b/hexacore-1c84b.appspot.com/o/Image%2F6y2HiDmxeueYcT3Hp87MyzE25lk2?alt=media&token=0bfbfdbb-2bbd-41a1-b760-5456c5a7c200"
+                width="36"
+                height="36"
+                
+                className=" rounded-full "
+                alt=''
+              />
+            </li>
+            <li>
+              <img
+                src="https://firebasestorage.googleapis.com/v0/b/hexacore-1c84b.appspot.com/o/Image%2F6y2HiDmxeueYcT3Hp87MyzE25lk2?alt=media&token=0bfbfdbb-2bbd-41a1-b760-5456c5a7c200"
+                width="36"
+                height="36"
+                
+                className=" rounded-full "
+                alt=''
+              />
+            </li>
+            <li>
+              <img
+                src="https://firebasestorage.googleapis.com/v0/b/hexacore-1c84b.appspot.com/o/Image%2F6y2HiDmxeueYcT3Hp87MyzE25lk2?alt=media&token=0bfbfdbb-2bbd-41a1-b760-5456c5a7c200"
+                width="36"
+                height="36"
+                
+                className=" rounded-full "
+                alt=''
+              />
+            </li>
+            <li>
+              <button
+                className="border border-dashed flex items-center w-9 h-9 border-gray-500 justify-center
+                rounded-full"
+              >
+                <PlusIcon className="w-5 h-5 text-gray-500" />
+              </button>
+            </li>
+          </ul>
         </div>
+
+        {/* Board columns */}
         {ready && (
           <DragDropContext onDragEnd={onDragEnd}>
-            <div className="flex w-full my-5 p-5">
-            <Droppable droppableId="droppable" type="BOARD" direction="horizontal">
-              {(provided, snapshot) => (
-              <div
-                ref={provided.innerRef}
-                className={`flex w-fit p-5 ${snapshot.isDraggingOver && "bg-green-100"}`}
-                {...provided.droppableProps}
-              >
-              {boardData.map((board, bIndex) =>(
-                 
-                    <Draggable type="BOARD" key={board.id} draggableId={board.id} index={bIndex} >
+            <div className="flex gap-5 flex-none w-fit my-5 overflow-x p-5">
+              {boardData.map((board, bIndex) => {
+                return (
+                  <div key={board.name}>
+                    <Droppable droppableId={bIndex.toString()}>
                       {(provided, snapshot) => (
                         <div
+                          {...provided.droppableProps}
                           ref={provided.innerRef}
-                          {...provided.draggableProps}
-                          {...provided.dragHandleProps}
-                          className="bg-transparent rounded-md p-3 m-3 mt-0 last:mb-0"
                         >
-                      <Board 
-                        board={board}
-                        bIndex={bIndex}
-                        members={members}/>
-                      </div>
+                          <div
+                            className={`bg-gray-100 w-30 rounded-md shadow-md
+                            flex flex-col relative
+                            ${snapshot.isDraggingOver && "bg-green-100"}`}
+                          >
+                            <span
+                              className="w-full h-1 bg-gradient-to-r from-pink-700 to-red-200
+                          absolute inset-x-0 top-0"
+                            ></span>
+                            <h4 className=" p-3 flex justify-between items-center mb-2">
+                              <span className="text-2xl text-gray-600">
+                                {board.name}
+                              </span>
+                              
+                            </h4>
+
+                            <div className="overflow-y-auto h-auto"
+                            style={{maxHeight:'calc(100vh - 290px)'}}>
+                              {board.items.length > 0 &&
+                                board.items.map((item, iIndex) => {
+                                  return (
+                                    <CardItem
+                                      key={item.id}
+                                      data={item}
+                                      index={iIndex}
+                                      
+                                    />
+                                  );
+                                })}
+                              {provided.placeholder}
+                            </div>
+                            
+                            {
+                              showForm && selectedBoard === bIndex ? (
+                                <div className="p-3">
+                                  <textarea className="border-gray-300 rounded focus:ring-purple-400 w-full" 
+                                  rows={3} placeholder="Task info" 
+                                  data-id={bIndex}
+                                  onKeyDown={(e) => onTextAreaKeyPress(e)}/>
+                                </div>
+                              ): (
+                                <button
+                                  className="flex justify-center items-center my-3 space-x-2 text-lg"
+                                  onClick={() => {setSelectedBoard(bIndex); setShowForm(true);}}
+                                >
+                                  <span>Add task</span>
+                                  <PlusCircleIcon className="w-5 h-5 text-gray-500" />
+                                </button>
+                              )
+                            }
+                          </div>
+                        </div>
                       )}
-                    </Draggable>
-              ))}
-              {provided.placeholder}  
-              </div>
-                  )}
-                </Droppable>
+                    </Droppable>
+                  </div>
+                );
+              })}
             </div>
           </DragDropContext>
         )}
       </div>
+    
   );
 }
