3437b117b4072bd1fa016ee49020781df505f86f
DahlGitHub <78036458+DahlGitHub@users.noreply.github.com>
2023-04-22 13:48:29 +0200

    Updated layout for poll creation

--- hexacore/components/Chat/CreatePoll.tsx
+++ hexacore/components/Chat/CreatePoll.tsx
@@ -37,23 +37,24 @@ export default ({id}) => {
     }
     return(
         <div className="mx-4">
-            <div>
-                <span>Question:</span>
-                <Input aria-label="question" value={question} onChange={(e)=>setQuestion(e.target.value)} clearable></Input>
+            <div className="flex-inline mb-3">
+                <label htmlFor="question" className="text-xs text-navy-700 dark:text-white font-bold">Question:</label>
+                <input aria-label="question" placeholder="Your question" className="mt-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" value={question} onChange={(e)=>setQuestion(e.target.value)}></input>
+                
             </div>
+            <label htmlFor="option" className="text-xs text-navy-700 dark:text-white font-bold">Options:</label>
             {options.map((option, i)=>{
                 return(
-                    <div className="flex">
-                        <span>Option {i+1}:</span>
-                        <Input aria-label="option" value={option.option} onChange={(e)=>updateOption(i, e.target.value)} clearable></Input>
-                        <Button onPress={()=>removeOption(i)} icon={<FontAwesomeIcon icon={faXmark}/>}/>
+                    <div className="flex space-x-2">
+                        <input aria-label="option" placeholder={"Option "+(i+1)} className="mt-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"  value={option.option} onChange={(e)=>updateOption(i, e.target.value)}></input>
+                        <button className="mt-2 w-20 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:text-white" onClick={()=>removeOption(i)}><FontAwesomeIcon icon={faXmark}/></button>
                     </div>
                 )
             })
             }
-            <Button onPress={()=>{
+            <button className="my-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:text-white" onClick={()=>{
                 setOptions(options.concat({option: "", votes: 0}))
-            }} icon={<FontAwesomeIcon icon={faSquarePlus}/>}/>
+            }}><FontAwesomeIcon icon={faSquarePlus}/><span className="text-xs mx-2 text-navy-700 dark:text-white">Add option</span></button>
             <Button onPress={createPoll} className="bg-sky-500">Create</Button>
         </div>
     )
