347f46ea0da6e4fded29e08de4f3c5a629d65495
DahlGitHub <78036458+DahlGitHub@users.noreply.github.com>
2023-04-22 13:10:22 +0200

    Poll tabs with styling

--- hexacore/components/Chat/Chat.tsx
+++ hexacore/components/Chat/Chat.tsx
@@ -7,6 +7,10 @@ import { doc, setDoc } from 'firebase/firestore';
 import { db } from '../../firebase'
 import { useEffect, useState } from 'react';
 import CreatePoll from './CreatePoll';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMessage } from '@fortawesome/free-regular-svg-icons';
+import { faDashboard, faPoll, faPollH, faSms, faSquarePollVertical, faVoteYea } from '@fortawesome/free-solid-svg-icons';
+import { faFacebookMessenger } from '@fortawesome/free-brands-svg-icons';
    
 
 export default ({chatID}) => {
@@ -16,6 +20,19 @@ export default ({chatID}) => {
   const [selectedButton, setSelectedButton] = useState('Message')
   const chat = chatID
 //const referenceCollection = collection(db, 'chat/'+chatID)
+
+function getButtonClassName(buttonName, selectedButton) {
+  let classNames = 'inline-block p-1 rounded-t-lg border-t border-l border-r';
+
+  if (selectedButton === buttonName) {
+    classNames += ' rounded-t-lg border-gray-400 bg-gray-100 px-2 border-b-none';
+  } else {
+    classNames += ' border-transparent hover:text-blue-600 dark:hover:text-gray-300 text-gray-500';
+  }
+
+  return classNames;
+}
+
 useEffect(()=>{
   if(!user){
     router.push('/login')
@@ -28,22 +45,32 @@ useEffect(()=>{
       setChatId(chatID)
     }
     }, [chatID])
-  return (
-    <div className=" flex 1 h-[calc(100vh-70px)] antialiased w-full">
-      <div className="flex flex-row h-full overflow-x-hidden">
-        <div className="flex flex-col flex-auto h-full p-2">
+    return (
+      <div className="flex flex-col h-[calc(100vh-70px)] antialiased w-full">
+        <div className="flex flex-row h-full overflow-x-hidden">
+          <div className="flex flex-col flex-auto h-full p-2">
             <div className="flex flex-col flex-auto flex-shrink-0 rounded-2xl divide-y-2 bg-gray-100 dark:bg-gray-800 h-full p-4">
               <MessageList id={chatId}/>
-              <div className="flex-none pt-1 ">
-                <div className='text-black space-x-1 divider-x mx-2'>
-                  <button onClick={()=>setSelectedButton('Message')}>Message</button>
-                  <button onClick={()=>setSelectedButton('Poll')}>Poll</button>
+              <div className="flex-none pt-1">
+                <div className="text-black space-x-1 divider-x bg-white rounded-lg">
+                <button
+                  className={getButtonClassName('Message', selectedButton)}
+                  onClick={() => setSelectedButton('Message')}
+                >
+                  <FontAwesomeIcon className='w-5 fa-xs' icon={faMessage}/> Message
+                </button>
+                <button
+                  className={getButtonClassName('Poll', selectedButton)}
+                  onClick={() => setSelectedButton('Poll')}
+                >
+                  <FontAwesomeIcon className='w-5 fa-sm' icon={faPoll}/>Poll
+                </button>
                 </div>
                 {selectedButton === 'Message' ? <MessageInput id={chatId}/> : <CreatePoll id={chatId}/>}
               </div>
             </div>
           </div>
         </div>
-    </div>
-  )
+      </div>
+    )
 }
