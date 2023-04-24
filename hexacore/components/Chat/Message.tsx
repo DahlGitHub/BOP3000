0a9395204de4da6d1fae8895e632824ecaef1a18
DahlGitHub <78036458+DahlGitHub@users.noreply.github.com>
2023-04-24 15:36:41 +0200

    dark chat mode

--- hexacore/components/Chat/Message.tsx
+++ hexacore/components/Chat/Message.tsx
@@ -45,8 +45,8 @@ export default ({index, message, id}) =>{
         }
       };
 
-    const textAreaClass = canEditMessage ? "bg-transparent" : "bg-gray-50 rounded-lg px-1 mr-2 pr-2";
-    const messageClass = canEditMessage ? "group-hover:bg-gray-200" : " bg-gray-200";
+    const textAreaClass = canEditMessage ? "bg-transparent" : "bg-gray-50 rounded-lg px-1 mr-2 pr-2 dark:bg-gray-600";
+    const messageClass = canEditMessage ? "group-hover:bg-gray-200 dark:group-hover:bg-gray-700" : " bg-gray-200 dark:bg-gray-700";
 
     return(
         
@@ -57,14 +57,14 @@ export default ({index, message, id}) =>{
     </div>
     <div className="flex-1 overflow-hidden">
       <div className="mt-4 flex items-center">
-        <span className="text-gray-800">{message.user.name}</span>
+        <span className="text-gray-800 dark:text-gray-100">{message.user.name}</span>
         <span className="text-gray-400 text-xs mx-3">{date.getDate() + "/" + (date.getMonth()+1) + " " +date.getHours()+ ":" + date.getMinutes()}</span>
       </div>
       <div className="mr-2">
         <textarea 
           ref={textAreaRef}
           rows={rows}
-          className={`resize-none w-full ${textAreaClass}`}
+          className={`resize-none w-full dark:text-gray-200 ${textAreaClass}`}
           onChange={(e) => setEditMessageValue(e.target.value)} 
           onKeyDown={onTextAreaKeyPress} 
           disabled={canEditMessage} 
@@ -72,14 +72,14 @@ export default ({index, message, id}) =>{
         />
         {!canEditMessage && (
           <div className="flex space-x-1">
-            <span className="text-xs">enter to 
+            <span className="text-xs dark:text-gray-100">enter to 
               <a className="px-1 hover:underline" onClick={async () => {
                 await updateDoc(doc(db, id+'/Messages/', message.messageId), {
                   message: editMessageValue,
                 });
                 setCanEditMessage(!false);
               }}><span className="text-blue-600">save</span></a></span>
-            <span className="text-xs">• escape to 
+            <span className="text-xs dark:text-gray-100">• escape to 
               <a className="px-1 hover:underline" onClick={() => {
                 setEditMessageValue(message.message.message);
                 setCanEditMessage(!false);
@@ -92,7 +92,7 @@ export default ({index, message, id}) =>{
       <Dropdown>
         <Dropdown.Trigger>
           <div className="tooltip" data-tip="More">
-            <button type="button" className='hidden group-hover:block dark:text-white text-[12px] mr-3 rounded bg-gray-300 p-1 border border-gray-400 shadow-lg'><FontAwesomeIcon className="fa-lg" icon={faEllipsisH}/></button>
+            <button type="button" className='hidden group-hover:block dark:text-white text-[12px] mr-3 rounded bg-gray-300 dark:bg-gray-800 p-1 border border-gray-400 shadow-lg'><FontAwesomeIcon className="fa-lg" icon={faEllipsisH}/></button>
           </div>
         </Dropdown.Trigger>
         <Dropdown.Menu
