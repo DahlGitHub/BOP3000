910c754eb465f9a9f0aefe83f3509ac4d3f137ae
Wohine <61017077+Wohine@users.noreply.github.com>
2023-04-24 17:52:33 +0200

    Logo on frontpage now from db

--- hexacore/components/Layout/NavBar.tsx
+++ hexacore/components/Layout/NavBar.tsx
@@ -26,7 +26,12 @@ return (
       <Navbar.Content hideIn="xs" variant="underline">
         <Link href="./">
           <Navbar.Brand>
-          <Image src={Logo} alt="Hexacore" width={35} height={35} />
+          <div className="dark:block">
+              <img src={"https://firebasestorage.googleapis.com/v0/b/hexacore-1c84b.appspot.com/o/hexacore.png?alt=media&token=0dc0577b-2ff1-4ff5-84b3-2e1896af25e0"} alt="Hexacore Logo"  className="ml-2 cursor-pointer w-10 h-10 transform transition duration-500 hover:scale-110"/>
+            </div>
+            <div className="dark:hidden">
+              <img src={"https://firebasestorage.googleapis.com/v0/b/hexacore-1c84b.appspot.com/o/darkmode%20logo.png?alt=media&token=8aacc126-c622-42c9-8c29-b964897dfc90"} alt="Hexacore Logo"  className="ml-2 cursor-pointer w-10 h-10 transform transition duration-500 hover:scale-110"/>
+            </div>
               <Text hideIn="xs">
                   Hexacore
               </Text>
