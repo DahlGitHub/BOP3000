a38841ef0695aea9a53a14dc5f5c3861453b8506
Wohine <61017077+Wohine@users.noreply.github.com>
2023-04-24 20:09:35 +0200

    Broken nextjs

--- hexacore/components/Home/WelcomeMessage.tsx
+++ hexacore/components/Home/WelcomeMessage.tsx
@@ -1,6 +1,5 @@
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../../firebase";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser } from "@fortawesome/free-regular-svg-icons";
 
 
