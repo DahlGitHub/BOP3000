8042fd5e7ed68b58b9bdb015a719f36ed60593d7
Wohine <61017077+Wohine@users.noreply.github.com>
2023-04-24 17:26:03 +0200

    fixed some avatar issues

--- hexacore/components/Home/Home.tsx
+++ hexacore/components/Home/Home.tsx
@@ -30,7 +30,7 @@ const Home = () => {
                 const userData = userDoc.data();
                 const element = (
                 <button key={doc.id} className="rounded flex items-center w-full px-5 py-2 transition-colors duration-200 dark:hover:bg-gray-800 gap-x-2 hover:bg-gray-100 focus:outline-none">
-                    <AvatarPicture picture={userData.picture} name={userData.name} />
+                    <AvatarPicture picture={userData.picture} name={userData.name} containerWidth={"10"} containerHeight={"10"}/>
                     <div className="text-left rtl:text-right">
                     <h1 className="text-sm font-medium text-gray-700 capitalize dark:text-white">{userData.name}</h1>
                     <p className="text-xs text-gray-500 dark:text-gray-400">{userData.email}</p>
