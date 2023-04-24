8426390d50abb0539523f8acb81d3619e4ec1716
DahlGitHub <78036458+DahlGitHub@users.noreply.github.com>
2023-04-24 17:30:50 +0200

    kanban example

--- hexacore/pages/dashboard/kanban.tsx
+++ hexacore/pages/dashboard/kanban.tsx
@@ -6,7 +6,7 @@ const kanban = () => {
   return (
     <DashboardLayout>
     <div className='max-w-fit'>
-      <Kanban/>
+      <Kanban id="/groups/a82bcf3fff364e71b2a8bb39903be3dd/kanbanid" membersId="/users" />
       </div>
     </DashboardLayout>
   )
