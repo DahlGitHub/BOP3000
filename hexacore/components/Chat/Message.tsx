5e1c0cc7093c8c91bcec0cd5f7aef2c65d0776d0
DahlGitHub <78036458+DahlGitHub@users.noreply.github.com>
2023-04-24 15:25:39 +0200

    Added cancel, and better layout

--- hexacore/components/Chat/Message.tsx
+++ hexacore/components/Chat/Message.tsx
@@ -12,7 +12,8 @@ export default ({index, message, id}) =>{
     const [rows, setRows] = useState(1)
     const textAreaRef = useRef(null);
     const date = message.message.sentAt.toDate()
-    
+    const [isEditing, setIsEditing] = useState(false);
+
     const deleteMessage = async () => {
         await deleteDoc(doc(db, id+'/Messages/', message.messageId))
     }
@@ -28,68 +29,93 @@ export default ({index, message, id}) =>{
     },[editMessageValue])
 
     const onTextAreaKeyPress = async (e) => {
-        if(e.keyCode === 13 && !e.shiftKey){
-            e.preventDefault()
-          if(editMessageValue.length === 0) {
-            setCanEditMessage(!false)
-          }
-          else {
+        if (e.key === 'Escape') {
+          setCanEditMessage(true);
+          setEditMessageValue(message.message.message);
+        } else if (e.key === 'Enter' && !e.shiftKey) {
+          e.preventDefault();
+          if (editMessageValue.length === 0) {
+            setCanEditMessage(true);
+          } else {
             await updateDoc(doc(db, id+'/Messages/', message.messageId), {
-                message: editMessageValue
-            })
-            setCanEditMessage(!false)
+              message: editMessageValue
+            });
+            setCanEditMessage(true);
           }
         }
-      }
+      };
+
+    const textAreaClass = canEditMessage ? "bg-transparent" : "bg-gray-50 rounded-lg px-1 mr-2 pr-2";
+    const messageClass = canEditMessage ? "group-hover:bg-gray-200" : " bg-gray-200";
+
     return(
         
-        <div key={index} className="p-3 rounded-lg group relative">
-        <div className="flex items-center justify-start flex-row group-hover:bg-gray-200 rounded-lg">
-          <img src={message.user.photo} className="object-cover w-10 h-10 rounded-full mx-2" alt=""/>
-          <div className="flex-1 overflow-hidden">
-            <div>
-              <span className="text-gray-800">{message.user.name}</span>
-              <span className="text-gray-400 text-xs mx-3">{date.getDate() + "/" + (date.getMonth()+1) + " " +date.getHours()+ ":" + date.getMinutes()}</span>
-            </div>
-            <input 
-              ref={textAreaRef} 
-              className="bg-transparent resize-none break-all" 
-              style={{ maxHeight: '200px' }} 
-              onChange={(e) => setEditMessageValue(e.target.value)} 
-              onKeyDown={onTextAreaKeyPress} 
-              disabled={canEditMessage} 
-              value={editMessageValue}
-            />
+<div key={index} className="px-2 py-0.5 rounded-lg group relative">
+  <div className={`flex items-center justify-start flex-row rounded-lg ${messageClass}`}>
+    <div className="top-0">
+    <img src={message.user.photo} className="object-cover w-12 h-12 rounded-full mx-2" alt=""/>
+    </div>
+    <div className="flex-1 overflow-hidden">
+      <div className="mt-4 flex items-center">
+        <span className="text-gray-800">{message.user.name}</span>
+        <span className="text-gray-400 text-xs mx-3">{date.getDate() + "/" + (date.getMonth()+1) + " " +date.getHours()+ ":" + date.getMinutes()}</span>
+      </div>
+      <div className="mr-2">
+        <textarea 
+          ref={textAreaRef}
+          rows={rows}
+          className={`resize-none w-full ${textAreaClass}`}
+          onChange={(e) => setEditMessageValue(e.target.value)} 
+          onKeyDown={onTextAreaKeyPress} 
+          disabled={canEditMessage} 
+          value={editMessageValue}
+        />
+        {!canEditMessage && (
+          <div className="flex space-x-1">
+            <span className="text-xs">enter to 
+              <a className="px-1 hover:underline" onClick={async () => {
+                await updateDoc(doc(db, id+'/Messages/', message.messageId), {
+                  message: editMessageValue,
+                });
+                setCanEditMessage(!false);
+              }}><span className="text-blue-600">save</span></a></span>
+            <span className="text-xs">• escape to 
+              <a className="px-1 hover:underline" onClick={() => {
+                setEditMessageValue(message.message.message);
+                setCanEditMessage(!false);
+              }}><span className="text-blue-600">cancel</span></a></span>
           </div>
-          <div className="absolute right-2.5 top-0">
-            
-            <Dropdown>
-                <Dropdown.Trigger>
-                    <div className="tooltip" data-tip="More">
-                    <button type="button" className='hidden group-hover:block dark:text-white text-[12px] mr-3 rounded bg-gray-300 p-1 border border-gray-400 shadow-lg'><FontAwesomeIcon className="fa-lg" icon={faEllipsisH}/>
-                    </button>
-           
-                    </div>
-                </Dropdown.Trigger>
-                <Dropdown.Menu
-                className="text-center p-1"
-                selectionMode="single"
-                css={{ $$dropdownMenuMinWidth: "100px" }}
-                onAction={(key)=>{
-                    if(key == 'edit'){
-                    setCanEditMessage(!canEditMessage)
-                    }
-                    if(key == 'delete'){
-                    deleteMessage()
-                    }
-                }}>
-                <Dropdown.Item icon={<FontAwesomeIcon icon={faPenToSquare}/>} key="edit">Edit</Dropdown.Item>
-                <Dropdown.Item color='error' icon={<FontAwesomeIcon icon={faTrash}/>} key='delete'>Delete</Dropdown.Item>
-                </Dropdown.Menu>
-            </Dropdown>
-        </div>
+        )}
+      </div>
     </div>
+    <div className="absolute right-2.5 top-0">
+      <Dropdown>
+        <Dropdown.Trigger>
+          <div className="tooltip" data-tip="More">
+            <button type="button" className='hidden group-hover:block dark:text-white text-[12px] mr-3 rounded bg-gray-300 p-1 border border-gray-400 shadow-lg'><FontAwesomeIcon className="fa-lg" icon={faEllipsisH}/></button>
+          </div>
+        </Dropdown.Trigger>
+        <Dropdown.Menu
+          className="text-center p-1"
+          selectionMode="single"
+          css={{ $$dropdownMenuMinWidth: "100px" }}
+          onAction={(key)=>{
+            if(key == 'edit'){
+              setCanEditMessage(!canEditMessage)
+            }
+            if(key == 'delete'){
+              deleteMessage()
+            }
+          }}>
+          <Dropdown.Item icon={<FontAwesomeIcon icon={faPenToSquare}/>} key="edit">Edit</Dropdown.Item>
+          <Dropdown.Item color='error' icon={<FontAwesomeIcon icon={faTrash}/>} key='delete'>Delete</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
     </div>
+  </div>
+</div>
+
+
       
     )
 }
