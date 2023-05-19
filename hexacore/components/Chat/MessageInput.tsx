adf772c1acdeecadbdc11be237d1a300f3511b88
Linrik <henrik-lindam@hotmail.com>
2023-05-19 14:15:33 +0200

    fjernet knapp som refresha nettsiden når du sendte melding

--- hexacore/components/Chat/MessageInput.tsx
+++ hexacore/components/Chat/MessageInput.tsx
@@ -53,13 +53,6 @@ export default ({id}) =>{
       <label htmlFor="chat" className="sr-only">Your message</label>
         <div className="flex items-center rounded-lg">
             <textarea id="chat" onKeyDown={sendMessage} onChange={type} value={message} rows={row} className="resize-none pl-4 h-10 focus:h-full break-words p-2 w-full text-sm text-gray-900 bg-white rounded-lg border bg-gray-100 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Type message..."></textarea>
-              {
-              // "Button doesn't work yet" 
-              }
-              <button onClick={sendMessage} value={message} className="inline-flex justify-center m-2 p-1 text-blue-600 rounded-full cursor-pointer hover:bg-blue-100 dark:text-blue-500 dark:hover:bg-gray-600">
-                <svg aria-hidden="true" className="w-6 h-6 rotate-90" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z"></path></svg>
-                <span className="sr-only">Send message</span>
-              </button>
         </div>
       </form>
     </div>
