9b23ec04e1697dec0bddb097e76f9b18b26dca87
DahlGitHub <78036458+DahlGitHub@users.noreply.github.com>
2023-06-03 16:49:37 +0200

    default darkmode bg

--- hexacore/components/Layout/Dashboard/DashboardLayout.tsx
+++ hexacore/components/Layout/Dashboard/DashboardLayout.tsx
@@ -12,7 +12,7 @@ const DashboardLayout = ({ children }) => {
   const [user, loading, error] = useAuthState(auth)
   return (
     <UserContext.Provider value={{user, loading, error}}>
-      <div className="min-h-screen">
+      <div className="min-h-screen dark:bg-gray-900">
       <DashboardHead />
       <div className="flex">
         <DashboardSidebar />
