5d241d3a5c7f4ac51b4b5d6aa5e50d4381c9b740
DahlGitHub <78036458+DahlGitHub@users.noreply.github.com>
2023-06-07 22:20:26 +0200

    Update TeamMenu.tsx

--- hexacore/components/Team/TeamMenu.tsx
+++ hexacore/components/Team/TeamMenu.tsx
@@ -267,15 +267,15 @@ const handleToolSelect = (toolName, type) => {
         </div>
 
         
-        <div className='grid place-items-center'>
-          <button type="button" onClick={handleModalOpen} className="text-white p-5 bg-[#24292F] hover:bg-[#24292F]/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30">
+        <div className='grid place-items-left'>
+          <button type="button" onClick={handleModalOpen} className="text-white mx-2 my-0.5 bg-[#24292F] hover:bg-[#24292F]/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30">
             <FontAwesomeIcon className='pr-2' icon={faFolderPlus}/>
             
             Create a team
           </button>
-          <button type="button" onClick={() => setShowTeamInvites(!showTeamInvites)} className="mx-5 text-white bg-[#24292F] hover:bg-[#24292F]/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30">
+          <button type="button" onClick={() => setShowTeamInvites(!showTeamInvites)} className="mx-2 my-0.5 text-white bg-[#24292F] hover:bg-[#24292F]/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30">
             <FontAwesomeIcon className='pr-2' icon={faEnvelope}/>
-            Show Teaminvites {inviteCount > 0 ? <span className='bg-red-500 text-white rounded-full px-2 py-1 text-xs font-bold ml-2'>{inviteCount}</span> : null}
+            Show Team invites {inviteCount > 0 ? <span className='bg-red-500 text-white rounded-full px-2 py-1 text-xs font-bold ml-2'>{inviteCount}</span> : null}
           </button>
         </div>
       </>
